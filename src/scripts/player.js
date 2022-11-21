8570037c505e3cada430203a2283f1a68f74b19b
rroland <alena_tumanova96@mail.ru>
2022-11-21 18:07:10 +0400

    change error icon, add cursor and hover to element button

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -7,14 +7,20 @@ let audioControls = {
     flag:           true,
 };
 
+let timerControls = {
+    sec:            0,
+    min:            0,
+    t:              null,
+};
+
 function changeDisplay(id, opened) {
     let display = opened ? 'flex' : 'none';
     document.querySelector(id).style.display = display;
 }
 
 function addErrorElement(audioObj) {
-    let main = document.getElementById('main');
-    let strError = document.createElement('output');
+    const main = document.getElementById('main');
+    const strError = document.createElement('output');
     strError.classList.add("str_error");
 
     if (audioObj.error.message == "MEDIA_ELEMENT_ERROR: Empty src attribute")
@@ -25,12 +31,12 @@ function addErrorElement(audioObj) {
         strError.value = "Incorrect link";
     else
         strError.value = "Please, enter correct link";
-    main.appendChild(strError);
+    main.append(strError);
 }
 
 function onSubmit() {
     // создание объекта Audio //
-    let linkAudio = document.getElementById('input');
+    const linkAudio = document.getElementById('input');
     let audioObj = new Audio(linkAudio.value);
     
     // установка громкости по умолчанию //
@@ -38,12 +44,12 @@ function onSubmit() {
     
     // если ссылка валидная //
     audioObj.addEventListener('canplay', () => {
-        let player = document.getElementById('player');
-        let source = document.getElementById('source');
-        let widgetPlayer = document.getElementById('widget_player');
-        let leftOfTheThumb = document.getElementById("track_line");
-        let trackLine = document.getElementById('widget_track_line');
-        let timerShow = document.getElementById('widget_time');
+        const player = document.getElementById('player');
+        const source = document.getElementById('source');
+        const widgetPlayer = document.getElementById('widget_player');
+        const leftOfTheThumb = document.getElementById("track_line");
+        const trackLine = document.getElementById('widget_track_line');
+        const timerShow = document.getElementById('widget_time');
 
         audioControls.trackPrevLine = leftOfTheThumb;
         audioControls.trackLine = trackLine;
@@ -99,7 +105,7 @@ function onSubmit() {
     // если произошла ошибка //
     audioObj.addEventListener('error', function() {
         // меняю стиль ввода //
-        let input_url = document.getElementById('input');
+        const input_url = document.getElementById('input');
         if (input_url.classList.contains("input-ok")) {
             input_url.classList.remove("input-ok");
             input_url.classList.add("input-err");
@@ -107,54 +113,25 @@ function onSubmit() {
 
         // добавляю иконку ошибки //
         if (!document.querySelector(".but_error")) {
-            let inputPlace = document.getElementById('input_place');
+            const inputPlace = document.getElementById('input_place');
             var but = document.createElement('button');
-            const item = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
-            
+
             but.classList.add("but_error");
-            item.classList.add("item_error");
-            item.setAttributeNS(null, "viewBox", "0 0 24 24");
-            item.setAttributeNS(null, "width", "24");
-            item.setAttributeNS(null, "height", "24");
-            item.setAttributeNS(null, "fill", "none");
-            
-            const path1 = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-            const path2 = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-            const circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
-            
-            path1.setAttributeNS(null, "d", "M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z");
-            path1.setAttributeNS(null, "stroke", "#C6A827");
-            path1.setAttributeNS(null, "stroke-width", "2");
-            path1.setAttributeNS(null, "stroke-linecap", "round");
-            path1.setAttributeNS(null, "stroke-linejoin", "round");
-            
-            path2.setAttributeNS(null, "d", "M12 8V12");
-            path2.setAttributeNS(null, "stroke", "#C6A827");
-            path2.setAttributeNS(null, "stroke-width", "2");
-            path2.setAttributeNS(null, "stroke-linecap", "round");
-            path2.setAttributeNS(null, "stroke-linejoin", "round");
-            
-            circle.setAttributeNS(null, "cx", "12");
-            circle.setAttributeNS(null, "cy", "16");
-            circle.setAttributeNS(null, "r", "0.5");
-            circle.setAttributeNS(null, "fill", "black");
-            circle.setAttributeNS(null, "stroke", "#C6A827");
-            
-            item.append(path1);
-            item.append(path2);
-            item.append(circle);
-            
-            but.appendChild(item);
-            inputPlace.appendChild(but);
+            inputPlace.append(but);
+
+            but.innerHTML = '<svg class="item_error" viewBox="0 0 24 24" width="24" height="24" fill="none">\
+            <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#C6A827" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">\
+            </path><path d="M12 8V12" stroke="#C6A827" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">\
+            </path><circle cx="12" cy="16" r="0.5" fill="black" stroke="#C6A827"></circle></svg>';
         }
 
         // добавляю подпись с ошибкой //
-        let strError = document.querySelector(".str_error");
+        const strError = document.querySelector(".str_error");
 
         if (strError)
             strError.remove();
-            addErrorElement(audioObj);
-        }, false);
+        addErrorElement(audioObj);
+    }, false);
 }
 
 function playBtn() {
@@ -178,8 +155,8 @@ function errorHiding() {
     if (this.classList.contains("input-err")) {
         this.classList.remove("input-err");
         this.classList.add("input-ok");
-        let butError = document.querySelector(".but_error");
-        let strError = document.querySelector(".str_error");
+        const butError = document.querySelector(".but_error");
+        const strError = document.querySelector(".str_error");
         butError.remove();
         strError.remove();
     }
@@ -198,8 +175,8 @@ function backButton() {
     stopwatch("reset");
     audioControls.trackPrevLine.style.width = "0px";
     audioControls.trackLine.value = "0";
-    let volumeRange = document.getElementById("widget_volume");
-    let leftOfThumb = document.getElementById("volume_line");
+    const volumeRange = document.getElementById("widget_volume");
+    const leftOfThumb = document.getElementById("volume_line");
     volumeRange.value = "40";
     leftOfThumb.style.width = (volumeRange.value * 2.52) + "px";
     audioControls.audio = null;
@@ -207,7 +184,7 @@ function backButton() {
 }
 
 function inputVolume() {
-    let leftOfThumb = document.getElementById("volume_line");
+    const leftOfThumb = document.getElementById("volume_line");
 
         audioControls.audio.volume = this.value / 100;
         leftOfThumb.style.width = (audioControls.volume.value * 2.52) + "px";
@@ -216,8 +193,8 @@ function inputVolume() {
 function inputTrackLine() {
     let currentSec = audioControls.audio.duration * this.value / 100;
     // timer update
-    min = parseInt(currentSec / 60);
-    sec = parseInt(currentSec % 60);
+    timerControls.min = parseInt(currentSec / 60);
+    timerControls.sec = parseInt(currentSec % 60);
     
     audioControls.audio.currentTime = currentSec;
     audioControls.trackPrevLine.style.width = (audioControls.trackLine.value * 5.8) + "px";
@@ -226,61 +203,56 @@ function inputTrackLine() {
 // начало загрузки страницы //
 document.addEventListener('DOMContentLoaded', () => {
     // кнопка submit //
-    let submitBtn = document.getElementById('submit');
+    const submitBtn = document.getElementById('submit');
     submitBtn.addEventListener('click', onSubmit);
     
     // скрытие отображения ошибки при новом вводе //
-    let input_url = document.getElementById('input');
+    const input_url = document.getElementById('input');
     input_url.addEventListener('input', errorHiding);    
 
     // реализация плеера //
-    let widgetPlayBtn = document.getElementById('widget_play');
+    const widgetPlayBtn = document.getElementById('widget_play');
     widgetPlayBtn.addEventListener('click', playBtn);
 
     // кнопка назад //
-    let backBtn = document.getElementById('back');
+    const backBtn = document.getElementById('back');
     backBtn.addEventListener('click', backButton);
 
     // громкость //
-    let volumeRange = document.getElementById("widget_volume");
+    const volumeRange = document.getElementById("widget_volume");
     audioControls.volume = volumeRange;
     volumeRange.addEventListener('input', inputVolume);
     
     // вручную меняем ползунок прогресса //
-    let trackLine = document.getElementById('widget_track_line');
+    const trackLine = document.getElementById('widget_track_line');
     trackLine.addEventListener('input', inputTrackLine);
 });
 
-var sec = 0;
-var min = 0;
-
 function add() {
-    sec += 0.1;
-    if (sec >= 60) {
-        sec = 0;
-        min++;
-        if (min >= 99) {
-            min = 0;
+    timerControls.sec += 0.1;
+    if (timerControls.sec >= 60) {
+        timerControls.sec = 0;
+        timerControls.min++;
+        if (timerControls.min >= 99) {
+            timerControls.min = 0;
         }
     }
     changeProgressLine();
-    audioControls.timerShow.textContent = (min > 9 ? min : "0" + min) + ":" + (sec > 9 ? Math.ceil(sec) : "0" + Math.ceil(sec));
+    audioControls.timerShow.textContent = (timerControls.min > 9 ? timerControls.min : "0" + timerControls.min) + ":" + (timerControls.sec > 9 ? Math.ceil(timerControls.sec) : "0" + Math.ceil(timerControls.sec));
 }
 
-
-let t;
 function stopwatch(value) {
     switch (value) {
         case 'play' :
-            t = setInterval(add, 100);
+            timerControls.t = setInterval(add, 100);
             break;
         case 'pause' :
-            clearInterval(t);
+            clearInterval(timerControls.t);
             break;
         case 'reset' :
-            clearInterval(t);
+            clearInterval(timerControls.t);
             audioControls.timerShow.textContent = "00:00";
-            sec = 0; min = 0;
+            timerControls.sec = 0; timerControls.min = 0;
             break;
         default:
             console.log('Undefined value');
